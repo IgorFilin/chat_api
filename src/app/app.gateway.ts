82568db77b8608a4755c5700aab5c8f2dfe00f35
IgorFilin <filinigor@yandex.ru>
2023-09-09 20:31:47 +0300

    Пофиксил отправку сообщений всем пользакам

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -13,13 +13,20 @@ import { Socket, Server } from 'socket.io';
 export class AppGateway {
   @WebSocketServer()
   server: Server;
-
+  clients = [];
   private logger: Logger = new Logger('WebSocket');
+
+  broadcastMessage(payload: any) {
+    for (const client of this.clients) {
+      const message = JSON.stringify(payload);
+      client.send(message);
+    }
+  }
 
   @SubscribeMessage('message')
   handleMessage(@MessageBody() body: any) {
     console.log(body);
-    this.server.emit('message', '123123'); // отправляем данные всем подключенным клиентам
+    this.broadcastMessage(body); // отправляем данные всем подключенным клиентам
   }
 
   afterInit(server: Server) {
@@ -33,6 +40,7 @@ export class AppGateway {
   }
 
   handleConnection(client: Socket, ...args: any[]) {
+    this.clients.push(client);
     console.log(`Client connected: ${client.id}`);
     this.logger.log(`Client connected: ${client.id}`);
   }
