ef2c5305d38990af4a506b33f8e9f7fe35991ede
IgorFilin <filinigor@yandex.ru>
2023-11-21 12:12:07 +0300

    Попытка фикса хостинга (2)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
-import * as cookieParser from 'cookie-parser';
+// import * as cookieParser from 'cookie-parser';
+
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
